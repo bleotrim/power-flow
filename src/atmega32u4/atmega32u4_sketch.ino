5afb3009e00ca1ecc2061fcca80b19e990a5e902
Leotrim <bleotrim@gmail.com>
2025-07-06 10:36:28 +0200

    added pro micro sketch

---
+++ src/atmega32u4/atmega32u4_sketch.ino
@@ -0,0 +1,104 @@
+const int STATUS_LED = 17;
+const int SWITCH_1 = 7;
+const int SWITCH_2 = 8;
+
+unsigned long previousMillis = 0;
+int step = 0;
+
+const String AUTH_TOKEN = "4F7D9B2A1C8E5G0H";
+bool authenticated = false;
+
+struct BlinkStep {
+  bool ledOn;
+  unsigned long duration;
+};
+
+const BlinkStep blinkPattern[] = {
+  {true,  300},
+  {false, 200},
+  {true,  300},
+  {false, 30000}
+};
+
+const int TOTAL_STEPS = sizeof(blinkPattern) / sizeof(blinkPattern[0]);
+
+void setup() {
+  pinMode(STATUS_LED, OUTPUT);
+  pinMode(SWITCH_1, OUTPUT);
+  pinMode(SWITCH_2, OUTPUT);
+  digitalWrite(STATUS_LED, LOW);
+  digitalWrite(SWITCH_1, LOW);
+  digitalWrite(SWITCH_2, LOW);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  handleBlink();
+  handleSerial();
+}
+
+void handleBlink() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= blinkPattern[step].duration) {
+    previousMillis = currentMillis;
+    digitalWrite(STATUS_LED, blinkPattern[step].ledOn ? HIGH : LOW);
+    step = (step + 1) % TOTAL_STEPS;
+  }
+}
+
+void handleSerial() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+
+    if (command.startsWith("login:")) {
+      String token = command.substring(6);
+      if (token == AUTH_TOKEN) {
+        authenticated = true;
+        Serial.println("AUTH:OK");
+      }
+      return;
+    }
+
+    if (!authenticated) {
+      return;
+    }
+
+    if (command == "status" && authenticated) {
+      bool statusSwitch_1 = digitalRead(SWITCH_1);
+      bool statusSwitch_2 = digitalRead(SWITCH_2);
+
+      if (statusSwitch_1 && !statusSwitch_2) {
+        Serial.println("SWITCH_1:ON, SWITCH_2:OFF");
+      } else if (!statusSwitch_1 && statusSwitch_2) {
+        Serial.println("SWITCH_1:OFF, SWITCH_2:ON");
+      } else if (!statusSwitch_1 && !statusSwitch_2) {
+        Serial.println("SWITCH_1:OFF, SWITCH_2:OFF");
+      } else if (statusSwitch_1 && statusSwitch_2) {
+        Serial.println("SWITCH_1:ON, SWITCH_2:ON");
+      }
+    }
+    else if (command == "swtch:1:on" && authenticated) {
+      if (digitalRead(SWITCH_2) == HIGH) {
+        Serial.println("CMD:ERROR");
+      } else {
+        digitalWrite(SWITCH_1, HIGH);
+        Serial.println("CMD:OK");
+      }
+    }
+    else if (command == "swtch:2:on" && authenticated) {
+      if (digitalRead(SWITCH_1) == HIGH) {
+        Serial.println("CMD:ERROR");
+      } else {
+        digitalWrite(SWITCH_2, HIGH);
+        Serial.println("CMD:OK");
+      }
+    }
+    else if (command == "swtch:off" && authenticated) {
+      digitalWrite(SWITCH_1, LOW);
+      digitalWrite(SWITCH_2, LOW);
+      Serial.println("CMD:OK");
+    }
+  }
+}
